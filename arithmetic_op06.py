2292a600958a72b11f6d006a511a6517fd966b87
Bekzod <bekzodmirsaidov91@gmail.com>
2022-03-11 17:25:55 +0500

    6-masala

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,7 +1,7 @@
 # Create a variable "a"  and assign it the value of the int 7.
 a=7
 # Create a variable "b"  and assign it the value of the int 3.
-b=7
+b=3
 # Create a variable "c"  and assign it the value of the int 5.
 c=5
 # Create a variable called "answer".
